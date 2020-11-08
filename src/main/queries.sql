f40c47e1c830591457205f376d172bcd5891e1b9
Iuliia Fieser <iuliiafieser@Iuliias-Mac-mini.local>
2020-11-07 22:18:44 -0600

    ready for submission

--- src/main/queries.sql
+++ src/main/queries.sql
@@ -1,8 +1,18 @@
-SELECT *
-FROM techjobs.job;
+PART 1
+
+SHOW COLUMNS FROM techjobs.job;
+
+PART 2
+SELECT name
+FROM techjobs.employer
+WHERE location = 'St. Louis City';
 
 
 PART 3
 DROP TABLE techjobs.job;
 
 PART 4
+SELECT techjobs.skill.name, techjobs.skill.description
+FROM techjobs.skill, techjobs.job, techjobs.job_skills
+WHERE techjobs.job_skills.jobs_id IS NOT NULL
+ORDER BY techjobs.job.name ASC;
